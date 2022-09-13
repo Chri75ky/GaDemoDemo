a63cd2b40a29161d3c11a0a02e140e2c5cef73a0
chri75ky <chri75ky@stud.kea.dk>
2022-09-13 10:59:59 +0200

    Fifth commit

--- src/main/java/com/example/databaseiskyen/DatabaseiskyenApplication.java
+++ src/main/java/com/example/databaseiskyen/DatabaseiskyenApplication.java
@@ -12,19 +12,7 @@ public class DatabaseiskyenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseiskyenApplication.class, args);
-		String url = "jdbc:mysql://52.138.137.228:3306/mysql";
-		String username = "root";
-		String password = "mypassword";
 
-		System.out.println("Connecting database...");
-
-		try (
-				Connection connection = DriverManager.getConnection(url, username, password)) {
-			System.out.println("Database connected!");
-		} catch (
-				SQLException e) {
-			throw new IllegalStateException("Cannot connect the database!", e);
-		}
 	}
 
 }
